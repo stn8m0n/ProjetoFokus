9abc1251fbdc496595dd53c6d11eab295c87b54d
Maicon Santana <msn-maicon@hotmail.com>
2023-09-02 04:44:55 -0300

    teste de validação de campo02

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const html = document.querySelector('htlm');
 const focoBt = document.querySelector('.app__card-button--foco');
-const curtoBt = document.querySelector('.app__card-button--curtos';)
+const curtoBt = document.querySelector('.app__card-button--curtos');
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto','foco' )
