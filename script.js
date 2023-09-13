36acabd80a01906aa6707ae5c5047c42e312f952
Maicon Santana <msn-maicon@hotmail.com>
2023-09-13 19:46:50 -0300

    fix images

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
-const banner = document.querySelector ('.app__imagem')
+const banner = document.querySelector ('.app__image')
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto','foco')
