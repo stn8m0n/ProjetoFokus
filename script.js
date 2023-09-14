58563cca8b66c8852fd4a4e1af226f628499c013
Maicon Santana <msn-maicon@hotmail.com>
2023-09-14 05:48:59 -0300

    textos02

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector ('.app__image')
-const titulo = document.querySelector ('app__title');
+const titulo = document.querySelector ('app__title')
 
 focoBt.addEventListener('click', () => {
     alterarContexto ('foco')
@@ -29,17 +29,20 @@ function alterarContexto (contexto){
         case "foco":
             titulo.innerHTML = `
             Otimize sua produtividade,<br>
-                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
             break;
         case "descanso-curto":
             titulo.innerHTML = `
             Que tal dar uma respirada?
-            <strong class="app__title-strong">faça uma pausa curta.</strong>`
+            <strong class="app__title-strong">faça uma pausa curta.</strong>
+            `
             break;
         case "descanso-longo":
             titulo.innerHTML =`
             Hora de voltar à superfície.
-            <strong class="app__title-strong"> Faça uma pausa longa.</strong>`
+            <strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
         default:
             break;
     }
